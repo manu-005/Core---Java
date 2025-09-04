0f1b409e713bdf74b0769b067012ece60565228b
manu-005 <140184323+manu-005@users.noreply.github.com>
2025-09-04 11:43:08 +0530

    AmazonRunner.java

--- src/com/xworkz/Hospital/Amazon/AmazonRunner.java
+++ src/com/xworkz/Hospital/Amazon/AmazonRunner.java
@@ -15,9 +15,30 @@ public class AmazonRunner {
         product2.setCatagory("Phones");
         product2.setPrice(18999.00);
 
+         Product product3 = new Product();
+        product3.setId(103);
+        product3.setName("Shirt");
+        product3.setCatagory("Fashion");
+        product3.setPrice(499.00);
+
+        Product product4 = new Product();
+        product4.setId(104);
+        product4.setName("Ceaners");
+        product4.setCatagory("Home Appliances");
+        product4.setPrice(399.00);
+
+        Product product5 = new Product();
+        product5.setId(105);
+        product5.setName("Jeans");
+        product5.setCatagory("Fashion");
+        product5.setPrice(999.00);
+
         Amazon app = new Amazon();
         app.addProduct(product1);
         app.addProduct(product2);
+        app.addProduct(product3);
+        app.addProduct(product4);
+        app.addProduct(product5);
 
         app.getProduct();
 
